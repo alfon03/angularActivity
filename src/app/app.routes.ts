3499995be89b1ed78f31d96c3946ec34de9d5253
Alfonso <desarrolloaplicacioneswebs0506@gmail.com>
2025-01-23 13:26:54 +0100

    subiendo api

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,21 @@
 import { Routes } from '@angular/router';
+import { TaskComponent } from './task/task.component';
+import { TaskDetailsComponent } from './task-details-component/task-details-component.component';
+import { PageNotFoundComponentComponent } from './page-not-found-component/page-not-found-component.component';
+import { MainComponent } from './main/main.component';
+export const routes: Routes = [
 
-export const routes: Routes = [];
+    {path: 'task', component: TaskComponent, children: [
+        {
+        path: ':id', 
+        component: TaskDetailsComponent, 
+        }
+      
+        ]
+    },
+    {path: 'index', component: MainComponent},
+    { path: 'taskDetails/:id', component: TaskDetailsComponent },
+    { path: 'task/:search', component: TaskDetailsComponent },
+    { path: '', redirectTo: 'index', pathMatch: 'full' },
+    { path: '**', component: PageNotFoundComponentComponent },
+];
